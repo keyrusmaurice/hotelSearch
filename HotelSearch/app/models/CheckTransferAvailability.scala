f1df6625d4d391037694c0c307b4f26f72f1363a
keyrus <keyrus.maurice@gmail.com>
2013-02-05 16:07:13 +0400

    Check Transfer Availability and Price
    done n test also

---
+++ HotelSearch/app/models/CheckTransferAvailability.scala
@@ -0,0 +1,51 @@
+package models
+
+import scala.xml._
+import java.util.Date
+
+class  CheckTransferAvailabilityRequest( sessionID : String, date : Date, 
+                          pickUpPoint  : String = "", dropOffPoint :String="", 
+                          adults : Int = 1, children : Int = 0 ) extends Request {
+  private val dateStr  = Constants.dateFormat.format(date) 
+  private val adultsStr = adults.toString
+  private val childrenStr = /*if (children == 0) "" else */children.toString
+
+  private val xml =
+    <Request>
+     <Function>CheckTransferAvailability</Function>
+      <SessionID>{sessionID}</SessionID>
+       <RequestDetails>
+	      <Date>{dateStr}</Date>
+	      <PickUpPoint>{pickUpPoint}</PickUpPoint>
+	      <DropOffPoint>{dropOffPoint}</DropOffPoint>
+	      <Adults>{adultsStr}</Adults>
+	      <Children>{childrenStr}</Children>        
+       </RequestDetails>
+    </Request>
+  
+  override def toString = xml.toString   
+}
+
+class ResponseCheckTransferAvailability(xmlString : String) extends Response(xmlString) { 
+  private val sess = xml \ "SessionID"
+  val sessionID = sess.text
+  private val env = xml \ "Environment"
+  val environment = env.text
+  
+  private val checkTransferAvailability = xml \ "CheckTransferAvailability"
+
+  val date = (checkTransferAvailability \ "Date").text
+  val pickUpPoint = (checkTransferAvailability \ "PickUpPoint").text  
+  val dropOffPoint = (checkTransferAvailability \ "DropOffPoint").text
+  val currency = (checkTransferAvailability \ "Currency").text
+
+  private val transferListXml = (checkTransferAvailability \ "TransferList" \ "Transfer").toList 
+  val transferList : List[Transfer] = transferListXml.map(new Transfer(_))
+}
+
+class Transfer(xml : scala.xml.Node) extends XmlResponse(xml){
+  val code = (xml \ "Code").text
+  val name = (xml \ "Name").text
+  val amount = (xml \ "Amount").text.toInt
+  val remarks = (xml \ "Remarks").text
+}
---
+++ HotelSearch/app/models/Test2.scala
@@ -0,0 +1,12 @@
+import models._
+
+object Test2{
+	def main(args : Array[String]) : Unit = {
+  val test = <Response> <Environment>PRODUCTION</Environment> <SessionID>OIWXZ5OUYU6+I6TAP5NTAW==</SessionID> <CheckTransferAvailability> <Date>2010-04-15</Date> <PickUpPoint>AJAA01</PickUpPoint> <DropOffPoint>DXBAPT</DropOffPoint> <Currency>USD</Currency>
+  <TransferList> <Transfer> <Code>SOMPOW</Code> <Name>Private Car/One Way</Name> <Amount>100</Amount> <Remarks></Remarks> </Transfer> <Transfer> <Code>HELOW</Code> <Name>Helicopter Transfer</Name> <Amount>200</Amount> <Remarks></Remarks> </Transfer> </TransferList> </CheckTransferAvailability> </Response>
+
+   val cta = new ResponseCheckTransferAvailability(test.toString)
+   val transferList = cta.transferList
+   for(t <- transferList) println(t.code)
+ }
+}
