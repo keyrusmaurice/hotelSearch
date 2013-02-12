a67b36fa35138e77906351859c6581a66f18316c
keyrusmaurice <keyrus.maurice@gmail.com>
2013-02-12 07:37:42 +0400

    refactored

--- HotelSearch/app/controllers/Application.scala
+++ HotelSearch/app/controllers/Application.scala
@@ -11,19 +11,19 @@ object Application extends Controller {
   def clear = Action{
     Ok("Bye").withNewSession
   }
+  
+  def getSession = {
+     val login = new RequestLogin(UserName, Password,AgentCode)    
+      val promis = WS.url(url).post(Map("RequestXML" -> Seq(login.toString)))    
+      val responseStr = promis.value.get.body
+      val newSession = new ResponseLogin(responseStr)
+      newSession.sessionID 
+  }
 
   def index = Action {
     request =>   
     val sessionSearch = request.session.get("connected")
-    val sessionValue = if (sessionSearch == None){ 
-      val login = new RequestLogin(UserName, Password,AgentCode)    
-      val promis = WS.url(url).post(Map("RequestXML" -> Seq(login.toString)))    
-      val responseStr = promis.value.get.body
-      val newSession = new ResponseLogin(responseStr)
-      newSession.sessionID      
-    }    
-    else    
-      sessionSearch.get     
+    val sessionValue = if (sessionSearch == None) getSession else sessionSearch.get     
     
     val str = sessionValue
     Ok(views.html.index(str)).withSession("connected" -> sessionValue)
@@ -32,25 +32,17 @@ object Application extends Controller {
   def checkhotelavailability = Action{
     request =>   
     request.session.get("connected").map {
-        connected => {
-      /*val login = new RequestLogin(UserName, Password,AgentCode)    
-      val promis = WS.url(url).post(Map("RequestXML" -> Seq(login.toString)))    
-      val responseStr = promis.value.get.body
-      val newSession = new ResponseLogin(responseStr)
-      val connected = newSession.sessionID  */    
+        connected => {          
       val ci = dateFormat.parse("2013-02-14")
       val co = dateFormat.parse("2013-02-15")
       val rha = new RequestHotelAvailability(connected,ci,co,hotelCityCode = Constants.hotelCityCodes("Dubai"))
       println(rha)      
       val promis2 = WS.url(url).post(Map("RequestXML" -> Seq(rha.toString)))
-      promis2.await(10000)
-          
-      val responseStr2 = promis2.value.get.body
-      println(responseStr2)
+      promis2.await(10000)          
+      val responseStr2 = promis2.value.get.body     
       Ok(views.html.index(responseStr2))      
     }}.getOrElse {
-    Unauthorized("Oops, you are not connected")
-   
+    Unauthorized("Oops, you are not connected")   
    } 
  }
   /*
